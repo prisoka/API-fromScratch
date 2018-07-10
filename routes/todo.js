5d060d63b9959a8a2d1bf7429b5a6114911fa547
Priscilla Okawa <prisoka@gmail.com>
2018-07-10 14:33:51 -0700

    GET one todo

--- routes/todo.js
+++ routes/todo.js
@@ -1,11 +1,11 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 //require knex:
-let knex = require('../db/knex');
+const knex = require('../db/knex');
 
 // get all todos
-router.get('/', function(req, res, next) {
+router.get('/', (req, res) => {
   // the client req all
   // the server get all from db
   knex('todo')
@@ -17,6 +17,28 @@ router.get('/', function(req, res, next) {
   })
   .catch((err) => {
     console.log('err', err);
+    res.status(500).send({error: {message: 'Something went wrong!'}}) // if can't find the req
+  })
+});
+
+// get one todo
+router.get('/:id', function(req, res, next) {
+  // the client req one single todo
+  const id = req.params.id;
+  // the server gets the given id and search for sgl item n DB
+  knex('todo')
+  .where('id', id) // 'id' --> table column
+  .then((data) => {
+    // if found, response data
+    if(data.length > 0){
+      res.status(200).send(data[0]) // [0] --> calls just (expected) SINGLE item, not whole array
+    } else {
+      res.status(404).send({error: {message: 'Not Found!'}})
+    }
+  })
+  .catch((err) => {
+    console.log('err', err);
+    // if not found: 404
     res.status(500).send({error: {message: 'Something went wrong!'}}) // if can't find the req
   })
 });
