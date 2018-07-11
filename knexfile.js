19fcd523b1edbc96d9ff6999d0238c5fc1855057
Priscilla Okawa <prisoka@gmail.com>
2018-07-10 23:24:18 -0700

    package.json to deploy

--- knexfile.js
+++ knexfile.js
@@ -9,7 +9,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL
+    connection: 'postgres://gsnbjybhqlsfdv:b2b6e5f36baa9b5570736e46765b0f4fc851889ffb0c62e03a94fa7cc548ae48@ec2-184-73-174-171.compute-1.amazonaws.com:5432/dr8pcljshbve7'
   }
 
 };
